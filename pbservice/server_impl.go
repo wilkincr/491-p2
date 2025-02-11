eb2a56304d77f29ce86929acd5c743c57711fc69
connorwilkinson11 <connorwilkinson11@outlook.com>
2025-02-11 14:13:25 -0500

    added retrying behavior

--- pbservice/server_impl.go
+++ pbservice/server_impl.go
@@ -2,7 +2,6 @@ package pbservice
 
 import (
 	"log"
-	"strconv"
 
 	"umich.edu/eecs491/proj2/viewservice"
 )
@@ -10,118 +9,151 @@ import (
 // additions to PBServer state.
 type PBServerImpl struct {
 	currentView viewservice.View
-	server      server
-}
-
-type server struct {
-	putter        chan<- OpArgs
-	appender      chan<- OpArgs
-	getter        chan<- OpArgs
-	replier       <-chan OpReply
-	sendPusher    chan<- interface{}
-	pushReplier   <-chan PushReply
-	receivePusher chan<- PushArgs
+	// channels for the goroutine
+	operator      chan OpArgs
+	replier       chan OpReply
+	sendPusher    chan interface{}
+	pushReplier   chan PushReply
+	receivePusher chan PushArgs
 	end           chan interface{}
 }
 
-func (s server) put(args OpArgs) OpReply {
-	// log.Println("Put called")
-	s.putter <- args
-	// log.Println("Put done")
-	return <-s.replier
+func (impl *PBServerImpl) doOperation(args OpArgs) OpReply {
+	impl.operator <- args
+	return <-impl.replier
 }
 
-func (s server) append(args OpArgs) OpReply {
-	s.appender <- args
-	return <-s.replier
+func (impl *PBServerImpl) sendPush() PushReply {
+	impl.sendPusher <- struct{}{}
+	return <-impl.pushReplier
 }
 
-func (s server) get(args OpArgs) OpReply {
-	s.getter <- args
-	return <-s.replier
-}
-
-func (s server) sendPush() PushReply {
-	s.sendPusher <- struct{}{}
-	return <-s.pushReplier
-}
-
-func (s server) receivePush(args PushArgs) {
-	s.receivePusher <- args
-}
-
-func (pb *PBServer) makeServer() server {
-	var result server
-
-	putter := make(chan OpArgs)
-	appender := make(chan OpArgs)
-	getter := make(chan OpArgs)
-	replier := make(chan OpReply)
-	sendPusher := make(chan interface{})
-	pushReplier := make(chan PushReply)
-	receivePusher := make(chan PushArgs)
-
-	end := make(chan interface{})
-	result.putter = putter
-	result.appender = appender
-	result.getter = getter
-	result.replier = replier
-	result.sendPusher = sendPusher
-	result.pushReplier = pushReplier
-	result.receivePusher = receivePusher
-	result.end = end
-
-	go func() {
-		database := make(map[string]string)
-		opcache := make(map[string]Result)
-		reply := &OpReply{Err: OK}
-		for {
-			select {
-			case args := <-putter:
-				database[args.Key] = args.Value
-				reply = &OpReply{Err: OK}
-				opcache[args.Client+":"+strconv.Itoa(args.SeqNo)] = Result{SeqNo: args.SeqNo, V: *reply}
-				replier <- *reply
-
-			case args := <-appender:
-				reply = &OpReply{Err: OK}
-				database[args.Key] += args.Value
-				opcache[args.Client+":"+strconv.Itoa(args.SeqNo)] = Result{SeqNo: args.SeqNo, V: *reply}
-				replier <- *reply
-
-			case args := <-getter:
-				log.Println("Get received: ", args)
-				value := database[args.Key]
-				log.Println("Get value: ", value)
-				reply = &OpReply{Err: OK, Value: value}
-				opcache[args.Client+":"+strconv.Itoa(args.SeqNo)] = Result{SeqNo: args.SeqNo, V: *reply}
-				replier <- *reply
-			case <-sendPusher:
-				var pushReply PushReply
-				log.Println("sendPush received")
-				log.Printf("Push: primary=%s, backup=%s\n", pb.me, pb.impl.currentView.Backup)
-				ok := call(pb.impl.currentView.Backup, "PBServer.Push", PushArgs{KVStore: database, OpCache: opcache, View: pb.impl.currentView}, &pushReply)
-				if !ok {
-					log.Println("Push failed error ", pushReply.Err)
-				}
-				pushReplier <- pushReply
-			case args := <-receivePusher:
-				log.Println("Pulling")
-				database = args.KVStore
-				log.Println(database["1"])
-				opcache = args.OpCache
-				pb.impl.currentView = args.View
-			}
-
-		}
-
-	}()
-	return result
+func (impl *PBServerImpl) receivePush(args PushArgs) {
+	impl.receivePusher <- args
 }
 
 // your pb.impl.* initializations here.
 func (pb *PBServer) initImpl() {
-	pb.impl.server = pb.makeServer()
+	// Create channels
+	pb.impl.operator = make(chan OpArgs)
+	pb.impl.replier = make(chan OpReply)
+	pb.impl.sendPusher = make(chan interface{})
+	pb.impl.pushReplier = make(chan PushReply)
+	pb.impl.receivePusher = make(chan PushArgs)
+	pb.impl.end = make(chan interface{})
+
+	// Start the goroutine
+	go func() {
+		database := make(map[string]string)
+		opcache := make(map[string]Result)
+
+		for {
+			select {
+			case args := <-pb.impl.operator:
+
+				if _, exists := opcache[args.Client]; exists && args.SeqNo <= opcache[args.Client].SeqNo {
+					log.Printf("Duplicate operation from %s\n", args.Client)
+					pb.impl.replier <- opcache[args.Client].V
+					continue
+				}
+				// This is the new, single point for all client operations.
+				reply := OpReply{Err: OK} // We'll fill this in.
+
+				// 1) Print debug logs (moved from Operation())
+				log.Printf("Operation called by %s for %s key=%s val=%s; current P=%s B=%s\n",
+					args.Source, args.Op, args.Key, args.Value,
+					pb.impl.currentView.Primary, pb.impl.currentView.Backup)
+				log.Println("I'm", pb.me)
+
+				// 2) Possibly forward to backup
+				var forwardReply OpReply
+				if pb.NeedForward() {
+					log.Println("Forwarding to backup:", pb.impl.currentView.Backup)
+					fargs := args
+					fargs.Source = pb.me
+					// Force retry until success
+					for {
+						ok := call(pb.impl.currentView.Backup, "PBServer.Operation", fargs, &forwardReply)
+						// Forward successful, break out of loop
+						if ok {
+							break
+						}
+						// Able to make connection with backup, but backup identifies split brain
+						if forwardReply.Err == ErrWrongServer {
+							reply.Err = ErrWrongServer
+							pb.impl.replier <- reply
+							continue
+						}
+
+					}
+				}
+
+				// 3) Check if error from forwarding
+				if forwardReply.Err == ErrWrongServer || pb.WrongServerOp(args) {
+					reply.Err = ErrWrongServer
+					pb.impl.replier <- reply
+					continue
+				}
+
+				// 4) Actually apply the operation locally
+				switch args.Op {
+				case "Put":
+					database[args.Key] = args.Value
+				case "Append":
+					database[args.Key] += args.Value
+				case "Get":
+					val := database[args.Key]
+					reply.Value = val
+				default:
+					reply.Err = "UnknownOp"
+				}
+
+				// If you want to store results in opcache, do so:
+				opcache[args.Client] = Result{SeqNo: args.SeqNo, V: reply}
+
+				// 5) Return final reply
+				pb.impl.replier <- reply
+
+			case <-pb.impl.sendPusher:
+				// Pushing logic remains the same
+				pargs := PushArgs{
+					KVStore: database,
+					OpCache: opcache,
+					View:    pb.impl.currentView,
+				}
+				var pushReply PushReply
+				log.Println("sendPush received")
+				log.Printf("Push: primary=%s, backup=%s\n",
+					pb.me, pb.impl.currentView.Backup)
+				log.Println(database["1"])
+
+				for {
+					ok := call(pb.impl.currentView.Backup, "PBServer.Push", pargs, &pushReply)
+					// Push successful, break out of loop
+					if ok {
+						break
+					}
+					if !ok {
+						log.Println("Push failed error", pushReply.Err)
+					}
+					// Able to make connection with backup, but backup identifies split brain
+					if pushReply.Err == ErrWrongServer {
+						pushReply.Err = ErrWrongServer
+						pb.impl.pushReplier <- pushReply
+						continue
+					}
+				}
+
+			case pushArgs := <-pb.impl.receivePusher:
+				log.Println("Pulling on backup")
+				database = pushArgs.KVStore
+				opcache = pushArgs.OpCache
+				pb.impl.currentView = pushArgs.View
+				log.Printf(database["1"])
+
+			}
+		}
+	}()
 }
 
 func (pb *PBServer) NeedForward() bool {
@@ -129,54 +161,25 @@ func (pb *PBServer) NeedForward() bool {
 }
 
 // On the backup side, if you receive an operation from
-func (pb *PBServer) WrongServer(args OpArgs) bool {
+func (pb *PBServer) WrongServerOp(args OpArgs) bool {
 	return (pb.impl.currentView.Primary != pb.me && args.Source != pb.impl.currentView.Primary) ||
 		(args.Source != args.Client && args.Source != pb.impl.currentView.Primary)
 }
 
+// func (pb *PBServer) WrongServerPush(args PushArgs) bool {
+// 	return (pb.impl.currentView.Primary && args.Source != pb.impl.currentView.Primary) ||
+// }
+
 // server Operation() RPC handler.
 func (pb *PBServer) Operation(args OpArgs, reply *OpReply) error {
-	// log.Println("Operation called by ", args.Source, " for ", args.Op, args.Key, args.Value, "current Primary is ", pb.impl.currentView.Primary, "current Backup is ", pb.impl.currentView.Backup)
-	//log.Println("I'm ", pb.me)
-	// log.Println("Operation received from ", args.Source)
-	forwardArgs := args
-	forwardArgs.Source = pb.me
-	var forwardReply OpReply
-	// First forward to backup if primary
-	if pb.NeedForward() {
-		log.Println("Forwarding to backup")
-		log.Println("Backup is ", pb.impl.currentView.Backup)
-		ok := call(pb.impl.currentView.Backup, "PBServer.Operation", forwardArgs, &forwardReply)
-		if !ok {
-			// log.Println("Forwarding to backup failed")
-			reply.Err = ErrWrongServer
-			return nil
-		}
-	}
-	// Check if error from forwarding with backup
-	if forwardReply.Err == ErrWrongServer || pb.WrongServer(args) {
-		// log.Println("Wrong server")
-		reply.Err = ErrWrongServer
-		return nil
-	}
-
-	switch args.Op {
-	case "Put":
-		*reply = pb.impl.server.put(args)
-	case "Append":
-		*reply = pb.impl.server.append(args)
-	case "Get":
-		*reply = pb.impl.server.get(args)
-	}
-
-	// log.Println(reply)
+	*reply = pb.impl.doOperation(args)
 	return nil
 }
 
 // server Push() RPC handler
 func (pb *PBServer) Push(args PushArgs, reply *PushReply) error {
 	log.Println("Push received")
-	pb.impl.server.receivePush(args)
+	pb.impl.receivePush(args)
 	return nil
 }
 
@@ -200,7 +203,7 @@ func (pb *PBServer) tick() {
 	}
 	if pb.NeedPush(latestView) {
 		pb.impl.currentView = latestView
-		pb.impl.server.sendPush()
+		pb.impl.sendPush()
 	} else {
 		pb.impl.currentView = latestView
 	}
